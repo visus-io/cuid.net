c7a42daf0b1a97d8c2d2cb5f5cbf94b92c9005a2
Alan Brault <alan.brault@visus.io>
2022-12-10 18:17:38 -0500

    task: implement public api verification tests
    Signed-off-by: Alan Brault <alan.brault@visus.io>

---
+++ tests/cuid.net.tests/ApiFacts.cs
@@ -0,0 +1,17 @@
+﻿namespace Xaevik.Cuid.Tests;
+
+using System.Runtime.CompilerServices;
+using PublicApiGenerator;
+
+[UsesVerify]
+public class ApiFacts
+{
+	[Fact]
+	[MethodImpl(MethodImplOptions.NoInlining)]
+	public async Task Cuid_NoBreakingChanges_Async()
+	{
+		var api = typeof(Cuid).Assembly.GeneratePublicApi();
+
+		await Verify(api);
+	}
+}
