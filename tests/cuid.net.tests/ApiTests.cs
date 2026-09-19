54f4c8381ab1e46dc83387ffb02651ce2e4780f2
Alan Brault <alan.brault@visus.io>
2026-09-19 09:05:34 -0400

    chore: fix flakey test
    Signed-off-by: Alan Brault <alan.brault@visus.io>

--- tests/cuid.net.tests/ApiTests.cs
+++ tests/cuid.net.tests/ApiTests.cs
@@ -7,6 +7,11 @@ using PublicApiGenerator;
 [ExcludeFromCodeCoverage]
 internal sealed class ApiTests
 {
+    // Skipped on net48: the net48 host resolves cuid.net's netstandard2.0 build, and Mono.Cecil
+    // (used by PublicApiGenerator) cannot resolve the netstandard facade assembly when hosted by
+    // .NET Framework. See https://github.com/jbevain/cecil/issues/901. net8.0/net10.0 already
+    // exercise the library's native builds, so API-surface regressions are still caught.
+#if !NET48
     [Test]
     [MethodImpl(MethodImplOptions.NoInlining)]
     public async Task PublicApi_HasNoBreakingChanges_Async()
@@ -23,4 +28,5 @@ internal sealed class ApiTests
 
         await Verify(api).ConfigureAwait(false);
     }
+#endif
 }
